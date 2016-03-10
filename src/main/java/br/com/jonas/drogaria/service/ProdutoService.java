53f9329c730c8eb7578d60f2d9528de0ea513629
Jonas <jonas.costa@icloud.com>
2016-03-10 14:53:51 -0300

    Metodos de Editar e excluir

--- src/main/java/br/com/jonas/drogaria/service/ProdutoService.java
+++ src/main/java/br/com/jonas/drogaria/service/ProdutoService.java
@@ -5,9 +5,12 @@ package br.com.jonas.drogaria.service;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
@@ -21,36 +24,80 @@ import br.com.jonas.drogaria.domain.Produto;
 
 @Path("produto")
 public class ProdutoService {
-	
-	//http://localhost:8081/Drogaria/rest/produto
+
+	// http://localhost:8081/Drogaria/rest/produto
 	@GET
 	public String listar() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		List<Produto> produtos = produtoDAO.listar("descricao");
-		
+
 		Gson gson = new Gson();
 		String produtosJson = gson.toJson(produtos);
-		
+
 		return produtosJson;
 	}
-	
-	//http://localhost:8081/Drogaria/rest/produto
+
+	// http://localhost:8081/Drogaria/rest/produto
 	@POST
 	public String salvar(String json) {
-		
-		//converte json para objeto
+
+		// converte json para objeto
 		Gson gson = new Gson();
 		Produto produto = gson.fromJson(json, Produto.class);
-		
+
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		produto = produtoDAO.merge(produto);
-		
-		//converte ojbeto em Json
+
+		// converte ojbeto em Json
 		String produtoJson = gson.toJson(produto);
 		return produtoJson;
-		
-		
+
 	}
-	
+
+	// http://localhost:8081/Drogaria/rest/produto/{codigo}
+	// http://localhost:8081/Drogaria/rest/produto/10 exemplo
+	@Path("{codigo}")
+	@GET
+	public String buscarPorCodigo(@PathParam("codigo") Long codigo) {
+
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		Produto produto = produtoDAO.buscarPorcodigo(codigo);
+
+		Gson gson = new Gson();
+		String produtoJson = gson.toJson(produto);
+
+		return produtoJson;
+	}
+
+	// http://localhost:8081/Drogaria/rest/produto
+	@DELETE
+	public String excluir(String json) {
+
+		Gson gson = new Gson();
+		Produto produto = gson.fromJson(json, Produto.class);
+
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		produto = produtoDAO.buscarPorcodigo(produto.getCodigo());
+
+		produtoDAO.excluir(produto);
+
+		String produtoJson = gson.toJson(produto);
+		return produtoJson;
+	}
+
+	// http://localhost:8081/Drogaria/rest/produto
+	@PUT
+	public String editar(String json) {
+		Gson gson = new Gson();
+		Produto produto = gson.fromJson(json, Produto.class);
+
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		produtoDAO.editar(produto);
+
+		String produtoJson = gson.toJson(produto);
+
+		return produtoJson;
+
+	}
 
 }

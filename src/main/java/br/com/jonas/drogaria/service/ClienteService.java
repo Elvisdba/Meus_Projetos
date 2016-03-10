5b2341cc6d6dc90d3b9d6db2e336217fcd383834
Jonas <jonas.costa@icloud.com>
2016-03-10 16:11:51 -0300

    Metodo buscarPorCodigo

--- src/main/java/br/com/jonas/drogaria/service/ClienteService.java
+++ src/main/java/br/com/jonas/drogaria/service/ClienteService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
@@ -20,17 +21,32 @@ import br.com.jonas.drogaria.domain.Cliente;
 
 @Path("cliente")
 public class ClienteService {
-	
+
 	// http://localhost:8081/Drogaria/rest/cliente
 	@GET
 	public String salvar() {
 		ClienteDAO clienteDAO = new ClienteDAO();
 		List<Cliente> clientes = clienteDAO.listar();
-		
+
 		Gson gson = new Gson();
 		String clientesJson = gson.toJson(clientes);
-		
+
 		return clientesJson;
+	}
+
+	// http://localhost:8081/Drogaria/rest/cliente/codigo
+	@Path("{codigo}")
+	@GET
+	public String buscarPorCodigo(@PathParam("codigo") Long codigo) {
+
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Cliente cliente = clienteDAO.buscarPorcodigo(codigo);
+
+		Gson gson = new Gson();
+		String clienteJson = gson.toJson(cliente);
+
+		return clienteJson;
+
 	}
 
 }

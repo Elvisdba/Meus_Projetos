7cb343a624b6debd3f223620f35b62be81bfd368
Jonas <jonas.costa@icloud.com>
2016-03-12 09:39:40 -0300

    criacao dos metodos salvar e buscarPorCodigo

--- src/main/java/br/com/jonas/drogaria/service/PessoaService.java
+++ src/main/java/br/com/jonas/drogaria/service/PessoaService.java
@@ -6,7 +6,9 @@ package br.com.jonas.drogaria.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
@@ -19,20 +21,51 @@ import br.com.jonas.drogaria.domain.Pessoa;
  */
 @Path("pessoa")
 public class PessoaService {
-	
+
 	// http://localhost:8081/Drogaria/rest/pessoa
 	@GET
 	public String listar() {
-		
+
 		PessoaDAO pessoaDAO = new PessoaDAO();
 		List<Pessoa> pessoas = pessoaDAO.listar();
-		
+
 		Gson gson = new Gson();
 		String pessoasJson = gson.toJson(pessoas);
-		
+
 		return pessoasJson;
-		
+
 	}
-	
+
+	// http://localhost:8081/Drogaria/rest/pessoa
+	@POST
+	public String salvar(String json) {
+
+		Gson gson = new Gson();
+		Pessoa pessoa = gson.fromJson(json, Pessoa.class);
+
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		pessoaDAO.merge(pessoa);
+
+		String pessoaJson = gson.toJson(pessoa);
+
+		return pessoaJson;
+
+	}
+
+	// http://localhost:8081/Drogaria/rest/pessoa/{codigo}
+	// http://localhost:8081/Drogaria/rest/pessoa/10 exemplo
+	@Path("{codigo}")
+	@GET
+	public String buscaPorCodigo(@PathParam("codigo") Long codigo) {
+
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		Pessoa pessoa = pessoaDAO.buscarPorcodigo(codigo);
+
+		Gson gson = new Gson();
+		String pessoaJson = gson.toJson(pessoa);
+
+		return pessoaJson;
+
+	}
 
 }

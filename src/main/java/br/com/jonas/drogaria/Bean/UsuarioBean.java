c5c69506f56dfd26bb1aeb8e396f6ff0da3afe15
Jonas <jonas.costa@icloud.com>
2016-03-12 13:20:37 -0300

    alteracao no metodo novo

--- src/main/java/br/com/jonas/drogaria/Bean/UsuarioBean.java
+++ src/main/java/br/com/jonas/drogaria/Bean/UsuarioBean.java
@@ -10,7 +10,6 @@ import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
-import br.com.jonas.drogaria.dao.PessoaDAO;
 import br.com.jonas.drogaria.dao.UsuarioDAO;
 import br.com.jonas.drogaria.domain.Pessoa;
 import br.com.jonas.drogaria.domain.Usuario;
@@ -22,41 +21,47 @@ public class UsuarioBean implements Serializable {
 	private Usuario usuario;
 	private List<Usuario> usuarios;
 	private List<Pessoa> pessoas;
-	
-	
-	
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
+
 	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
-	
+
 	public void novo() {
 		try {
 			usuario = new Usuario();
-			
-			PessoaDAO pessoaDAO = new PessoaDAO();
-			pessoas = pessoaDAO.listar("nome");
+
+			// PessoaDAO pessoaDAO = new PessoaDAO();
+			// pessoas = pessoaDAO.listar("nome");
+
+			PessoaBean pessoaBean = new PessoaBean();
+			pessoas = pessoaBean.listarComRetorno();
+
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Erro ao tentar listar pessoas");
 			e.printStackTrace();
 		}
 	}
-	
+
 	@PostConstruct
 	public void listar() {
 		try {
@@ -67,35 +72,35 @@ public class UsuarioBean implements Serializable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void salvar() {
-		
+
 		try {
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			usuarioDAO.merge(usuario);
-			
+
 			novo();
-			
+
 			usuarios = usuarioDAO.listar();
 			Messages.addFlashGlobalInfo("Usuario Armazenado com sucesso");
 		} catch (RuntimeException e) {
 			Messages.addFlashGlobalError("Erro ao tentar salvar Usuario");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void editar(ActionEvent evento) {
 		usuario = (Usuario) evento.getComponent().getAttributes().get("usuarioSelecionado");
 	}
-	
+
 	public void excluir(ActionEvent evento) {
 		try {
 			usuario = (Usuario) evento.getComponent().getAttributes().get("usuarioSelecionado");
-			
+
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			usuarioDAO.excluir(usuario);
-			
+
 			usuarios = usuarioDAO.listar();
 			Messages.addFlashGlobalInfo("Usuario Excluido com sucesso");
 		} catch (RuntimeException e) {
@@ -103,6 +108,5 @@ public class UsuarioBean implements Serializable {
 			e.printStackTrace();
 		}
 	}
-	
 
 }

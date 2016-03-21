06984875500f06879a6af46e7f3c7e99824c802a
Jonas <jonas.costa@icloud.com>
2016-03-21 17:01:00 -0300

    criacao da classe VendaBean

---
+++ src/main/java/br/com/jonas/drogaria/Bean/VendaBean.java
@@ -0,0 +1,60 @@
+package br.com.jonas.drogaria.Bean;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.omnifaces.util.Messages;
+
+import com.google.gson.Gson;
+
+import br.com.jonas.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@SessionScoped
+public class VendaBean implements Serializable {
+
+	private List<Produto> produtos;
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	@PostConstruct
+	public void listar() {
+		try {
+			
+			//usando service
+			Client client = ClientBuilder.newClient();
+			WebTarget webTarget = client.target("http://localhost:8081/Drogaria/rest/produto");
+			
+			String json = webTarget.request().get(String.class);
+			
+			Gson gson = new Gson();
+			Produto[] vetorProduto = gson.fromJson(json, Produto[].class);
+			produtos = Arrays.asList(vetorProduto);
+			
+
+			
+//			ProdutoDAO produtoDAO = new ProdutoDAO();
+//			produtos = produtoDAO.listar();
+
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao tentar listar Produtos");
+			e.printStackTrace();
+		}
+	}
+	
+}

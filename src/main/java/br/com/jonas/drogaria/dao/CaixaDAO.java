dd9c7b1244185b2fe948d3a7537df51ca5c5eff2
Jonas <jonas.costa@icloud.com>
2016-08-09 22:04:43 -0300

    CaixaDAO

---
+++ src/main/java/br/com/jonas/drogaria/dao/CaixaDAO.java
@@ -0,0 +1,36 @@
+package br.com.jonas.drogaria.dao;
+
+import java.util.Date;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.jonas.drogaria.domain.Caixa;
+import br.com.jonas.drogaria.util.HibernateUtil;
+
+public class CaixaDAO extends GenericDAO<Caixa>{
+	
+	public Caixa buscar(Date dataAbertura){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		
+		try {
+			
+			Criteria consulta = sessao.createCriteria(Caixa.class);
+			consulta.add(Restrictions.eq("dataAbertura", dataAbertura));
+			Caixa resultado = (Caixa) consulta.uniqueResult();
+			
+			return resultado;
+			
+		} catch (RuntimeException erro) {
+			
+			
+			throw erro;
+		}finally{
+			sessao.close();
+		}
+		
+	}
+
+}

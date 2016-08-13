32f20e11a6bb3bd8ed163381745812ff351497f2
Jonas <jonas.costa@icloud.com>
2016-08-13 18:10:58 -0300

    atributo senha sem criptografia

--- src/main/java/br/com/jonas/drogaria/domain/Usuario.java
+++ src/main/java/br/com/jonas/drogaria/domain/Usuario.java
@@ -21,6 +21,9 @@ public class Usuario extends GenericDomain {
 	@Column(length = 32, nullable = false)
 	private String senha;
 
+	@Transient
+	private String senhaSemCriptografia;
+
 	@Column(nullable = false)
 	private Character tipo;
 
@@ -68,6 +71,14 @@ public class Usuario extends GenericDomain {
 
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	public String getSenhaSemCriptografia() {
+		return senhaSemCriptografia;
+	}
+
+	public void setSenhaSemCriptografia(String senhaSemCriptografia) {
+		this.senhaSemCriptografia = senhaSemCriptografia;
 	}
 
 	// nao sera mapeado
